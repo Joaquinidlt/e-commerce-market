40f947d1e6aa7454865ed3e28b388782b6b179c5
Joaquin Ignacio De la Torre <joaquindlt16@gmail.com>
2023-04-13 18:16:43 +0200

    New Update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ### `npm start`
 
 ### `npm test`
+
+### `important` you need to have the last version of node installed. `nvm use v19.9.0 `
+
+### `.env stripe keys` login in `https://dashboard.stripe.com/test/dashboard` and there you can take the `REACT_APP_STRIPE_PUBLISHABLE_KEY` and `STRIPE_SECRET_KEY`
+
+### `netlify login`
+
+### `netlify dev` "Remember that if you want to use the payment method, you need to use the `netlify dev` command, then that will work."
--- src/Components/button/button.styles.jsx
+++ src/Components/button/button.styles.jsx
@@ -18,6 +18,7 @@ export const BaseButton = styled.button`
   cursor: pointer;
   display: flex;
   justify-content: center;
+  align-items: center;
   box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.3);
   &:hover {
     background-color: white;
@@ -56,6 +57,26 @@ export const InvertedButton = styled(BaseButton)`
     background-color: #a01c32;
     color: white;
     border: none;
+  }
+`;
+export const ButtonSpinner = styled.div`
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  border: 3px solid rgba(195, 195, 195, 0.6);
+  border-radius: 50%;
+  border-top-color: #636767;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
   }
 `;
 
